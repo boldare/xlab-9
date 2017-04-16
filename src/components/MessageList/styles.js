fbf685701e9358e5ced5d1927384e2ad83ee02ad
Patryk Domaleczny <patryk.dom15@gmail.com>
2017-04-16 17:34:21 +0200

    styles deletion

--- src/components/MessageList/styles.js
+++ src/components/MessageList/styles.js
@@ -18,43 +18,9 @@ export const styles = StyleSheet.create({
     marginTop: 20,
     marginRight: 40,
   },
-  userListItem: {
-    marginRight: 0,
-    marginLeft: 25,
-    justifyContent: 'flex-end',
-  },
-  listItemBox: {
-    flex: 0,
-    flexDirection: 'column',
-    marginLeft: 15,
-  },
-  listItemTextBox: {
-    flexGrow: 1,
-    backgroundColor: '#C0DFEA',
-    borderRadius: 15,
-    padding: 10,
-    marginTop: 10,
-  },
-  userTextBox: {
-    backgroundColor: '#FF5967',
-  },
-  listItemLabel: {
-    fontSize: 14,
-  },
-  listItemOwner: {
-    fontSize: 12,
-    opacity: .7,
-  },
   loader: {
     height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  userImage: {
-    width: 30,
-    height: 30,
-    margin: 5,
-    borderRadius: 15,
-    backgroundColor: Platform.OS === 'ios' ? '#A4C2EA' : 'rgba(0,0,0,0)',
   },
 })
